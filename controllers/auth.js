1055b9cb755ef3b3221c5e12fdc08351deddf891
Edison Loor <edison.david.loor@gmail.com>
2021-12-08 16:07:31 -0500

    google singin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 const { response, json } = require("express");
 const bcryptjs = require ('bcryptjs');
 const Usuario= require ('../models/usuario');
-const { validationResult } = require("express-validator");
+const { validationResult, body } = require("express-validator");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res=response)=>{
 
@@ -48,6 +49,59 @@ const login = async (req, res=response)=>{
     
 }
 
+const googleSingIn = async (req,res=response)=>{
+    const {id_token}=req.body;
+
+    try {
+        const googleUser = await googleVerify(id_token);
+        
+        
+        //console.log(id_token);
+        
+        let usuario =await Usuario.findOne ({email:googleUser.email});
+        
+        if (!usuario){
+            // tengo que crear usuario
+            const data ={
+                correo: googleUser.email,
+                nombre: googleUser.name,
+                password: ':P',
+                img: googleUser.picture,
+                rol: 'ADMIN_ROLE',
+                google: true
+            };
+            
+            
+            usuario = new Usuario ( data );
+            await usuario.save();
+            
+        }
+        
+        // si el usuario en DB
+        if (!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+        
+        //generar el JWT
+        const token = await generarJWT (usuario.id);
+        
+        res.json ({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            msg: 'Token de google no es valido'
+        })
+    }
+    
+}
+
 module.exports={
-    login
+    login,
+    googleSingIn
 }
